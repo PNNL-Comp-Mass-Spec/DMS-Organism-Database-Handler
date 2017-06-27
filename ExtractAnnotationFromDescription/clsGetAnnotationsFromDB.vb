484bc6ba3f41462020608051b9ba8a2ffc409212
alchemistmatt <matthew.monroe@pnnl.gov>
2017-06-26 19:37:28 -0700

    Tabs to spaces

--- ExtractAnnotationFromDescription/clsGetAnnotationsFromDB.vb
+++ ExtractAnnotationFromDescription/clsGetAnnotationsFromDB.vb
@@ -13,102 +13,102 @@ Friend Class clsGetAnnotationsFromDB
         Me.m_ConnectionString = PSConnectionString
     End Sub
 
-	Function GetAnnotationDetails(ProteinCollectionID As Integer) As clsAnnotationInfo
-		Me.m_DatabaseHelper = New TableManipulationBase.clsDBTask(Me.m_ConnectionString, True)
+    Function GetAnnotationDetails(ProteinCollectionID As Integer) As clsAnnotationInfo
+        Me.m_DatabaseHelper = New TableManipulationBase.clsDBTask(Me.m_ConnectionString, True)
 
-		Dim CollectionName As String = String.Empty
+        Dim CollectionName As String = String.Empty
 
 
-		Dim SQLStatement As String
-		Dim dr As DataRow
-		Dim foundrows() As DataRow
+        Dim SQLStatement As String
+        Dim dr As DataRow
+        Dim foundrows() As DataRow
 
-		Dim tmpRefID As Integer
-		Dim tmpName As String
-		Dim tmpDesc As String
-		Dim tmpProtID As Integer
-		Dim tmpNameAuthID As Integer
+        Dim tmpRefID As Integer
+        Dim tmpName As String
+        Dim tmpDesc As String
+        Dim tmpProtID As Integer
+        Dim tmpNameAuthID As Integer
 
-		' FYI: The constructor for clsAnnotationInfo() doesn't use CollectionName or ProteinCollectionID at present
+        ' FYI: The constructor for clsAnnotationInfo() doesn't use CollectionName or ProteinCollectionID at present
         Dim info As New clsAnnotationInfo()
 
-		Dim tmpNameTable As DataTable
-		Dim tmpAuthorityTable As DataTable
-		Dim tmpAnnotationGroupTable As DataTable
-		Dim tmpAnnotationTable As DataTable
+        Dim tmpNameTable As DataTable
+        Dim tmpAuthorityTable As DataTable
+        Dim tmpAnnotationGroupTable As DataTable
+        Dim tmpAnnotationTable As DataTable
 
-		'Get Protein Collection Name
+        'Get Protein Collection Name
         SQLStatement = "SELECT TOP 1 Name FROM V_Collection_Picker " &
                             "WHERE ID = " & ProteinCollectionID
-		tmpNameTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
-		CollectionName = tmpNameTable.Rows(0).Item("Name").ToString
+        tmpNameTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
+        CollectionName = tmpNameTable.Rows(0).Item("Name").ToString
 
-		tmpNameTable.Clear()
-		tmpNameTable = Nothing
+        tmpNameTable.Clear()
+        tmpNameTable = Nothing
 
-		'Get Naming Authority Lookup
+        'Get Naming Authority Lookup
 
-		SQLStatement = "SELECT Authority_ID, Name FROM T_Naming_Authorities"
-		tmpAuthorityTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
+        SQLStatement = "SELECT Authority_ID, Name FROM T_Naming_Authorities"
+        tmpAuthorityTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
 
-		foundrows = tmpAuthorityTable.Select("")
-		For Each dr In foundrows
+        foundrows = tmpAuthorityTable.Select("")
+        For Each dr In foundrows
             info.AddAuthorityNameToLookup(DirectCast(dr.Item("Authority_ID"), Int32),
                 DirectCast(dr.Item("Name"), String))
-		Next
+        Next
 
-		tmpAuthorityTable.Clear()
-		tmpAuthorityTable = Nothing
+        tmpAuthorityTable.Clear()
+        tmpAuthorityTable = Nothing
 
 
-		'Get Annotation Group Information
+        'Get Annotation Group Information
 
         SQLStatement = "SELECT Annotation_Group, Authority_ID " &
             "FROM T_Annotation_Groups " &
             "WHERE Protein_Collection_ID = " & ProteinCollectionID.ToString
 
-		tmpAnnotationGroupTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
+        tmpAnnotationGroupTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
 
-		For Each dr In tmpAnnotationGroupTable.Rows
+        For Each dr In tmpAnnotationGroupTable.Rows
             info.AddAnnotationGroupLookup(
                 DirectCast(dr.Item("Annotation_Group"), Int32),
                 DirectCast(dr.Item("Authority_ID"), Int32))
-		Next
+        Next
 
 
 
-		'Get Collection Member Primary Information
+        'Get Collection Member Primary Information
 
-		'Get Reference_ID, Name, Description, Protein_ID, Protein_Collection_ID, 
-		'   Authority_ID, Annotation_Group_ID
+        'Get Reference_ID, Name, Description, Protein_ID, Protein_Collection_ID,
+        '   Authority_ID, Annotation_Group_ID
         SQLStatement = "SELECT * FROM V_Protein_Collection_Members " &
             "WHERE Protein_Collection_ID = " & ProteinCollectionID.ToString &
             " AND Annotation_Group_ID = 0"
-		tmpAnnotationTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
+        tmpAnnotationTable = Me.m_DatabaseHelper.GetTable(SQLStatement)
 
 
-		foundrows = tmpAnnotationTable.Select("")
+        foundrows = tmpAnnotationTable.Select("")
 
-		For Each dr In foundrows
-			tmpRefID = DirectCast(dr.Item("Reference_ID"), Int32)
-			tmpName = DirectCast(dr.Item("Name"), String)
-			tmpDesc = DirectCast(dr.Item("Description"), String)
-			tmpProtID = DirectCast(dr.Item("Protein_ID"), Int32)
-			tmpNameAuthID = DirectCast(dr.Item("Annotation_Type_ID"), Int32)
+        For Each dr In foundrows
+            tmpRefID = DirectCast(dr.Item("Reference_ID"), Int32)
+            tmpName = DirectCast(dr.Item("Name"), String)
+            tmpDesc = DirectCast(dr.Item("Description"), String)
+            tmpProtID = DirectCast(dr.Item("Protein_ID"), Int32)
+            tmpNameAuthID = DirectCast(dr.Item("Annotation_Type_ID"), Int32)
 
             info.AddPrimaryAnnotation(
                 tmpProtID, tmpName, tmpDesc, tmpRefID, tmpNameAuthID)
-		Next
+        Next
 
-		tmpAnnotationTable.Clear()
-		tmpAnnotationTable = Nothing
+        tmpAnnotationTable.Clear()
+        tmpAnnotationTable = Nothing
 
 
-		'Get Additional Annotations
-		SQLStatement = "SELECT "
+        'Get Additional Annotations
+        SQLStatement = "SELECT "
 
-		Return info
+        Return info
 
-	End Function
+    End Function
 
 End Class
