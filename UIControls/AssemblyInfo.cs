5655ec96e3b0f6106d9bf378aba1ee97cc4608c4
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-03 16:02:10 -0700

    Add UIControls project (decompiled from source)

---
+++ UIControls/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("UIControls")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PNNL")]
+[assembly: AssemblyProduct("UIControls")]
+[assembly: AssemblyCopyright("Copyright © PNNL 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
