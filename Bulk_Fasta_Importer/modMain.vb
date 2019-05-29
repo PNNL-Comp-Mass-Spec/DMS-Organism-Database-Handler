819a3ea0f2bacb1a1b9563826069cdd0230ce684
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-29 16:05:52 -0700

    Use wrap long lines when displaying syntax

--- Bulk_Fasta_Importer/modMain.vb
+++ Bulk_Fasta_Importer/modMain.vb
@@ -11,7 +11,7 @@ Imports ValidateFastaFile
 ''' </summary>
 Module modMain
 
-    Public Const PROGRAM_DATE As String = "May 9, 2019"
+    Public Const PROGRAM_DATE As String = "May 29, 2019"
 
     Private mInputFilePath As String
     Private mPreviewMode As Boolean
