b421f79d23673ca6ec3236f717343f5a7c460425
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2021-03-08 18:54:51 -0800

    Update csproj files and AssemblyInfo files

--- Bulk_Fasta_Importer/Properties/AssemblyInfo.cs
+++ Bulk_Fasta_Importer/Properties/AssemblyInfo.cs
@@ -1,34 +1,34 @@
-﻿Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bulk_Fasta_Importer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PNNL")]
+[assembly: AssemblyProduct("Bulk_Fasta_Importer")]
+[assembly: AssemblyCopyright("Copyright © PNNL 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-' Review the values of the assembly attributes
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
 
-<Assembly: AssemblyTitle("Bulk_Fasta_Importer")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("PNNL")> 
-<Assembly: AssemblyProduct("Bulk_Fasta_Importer")> 
-<Assembly: AssemblyCopyright("Copyright © PNNL 2014")> 
-<Assembly: AssemblyTrademark("")> 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a8c7469-8e84-424d-806f-99f9f8c2c7b2")]
 
-<Assembly: ComVisible(False)>
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("1a8c7469-8e84-424d-806f-99f9f8c2c7b2")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("1.0.*")> 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
--- Protein_Importer/Properties/AssemblyInfo.cs
+++ Protein_Importer/Properties/AssemblyInfo.cs
@@ -1,32 +1,35 @@
-Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 
-' Review the values of the assembly attributes
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
 
-<Assembly: AssemblyTitle("")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("")> 
-<Assembly: AssemblyCopyright("")> 
-<Assembly: AssemblyTrademark("")> 
-<Assembly: CLSCompliant(True)> 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9AA3F825-A27C-4A86-BFE5-7515DBC36D27")]
 
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("9AA3F825-A27C-4A86-BFE5-7515DBC36D27")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-
-<Assembly: AssemblyVersion("1.0.*")> 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
