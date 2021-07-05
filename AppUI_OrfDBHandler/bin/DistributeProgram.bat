2b0ef096830d57731beecca3fa22184ed9755710
alchemistmatt <matthew.monroe@pnnl.gov>
2021-07-04 22:26:04 -0700

    Add batch file

---
+++ AppUI_OrfDBHandler/bin/DistributeProgram.bat
@@ -0,0 +1,7 @@
+xcopy *.exe \\proto-2\PAST\Software\PrismSeqUploader\ /D /Y
+xcopy *.dll \\proto-2\PAST\Software\PrismSeqUploader\ /D /Y
+
+xcopy *.exe \\floyd\software\PrismSeq_Uploader\ /D /Y
+xcopy *.dll \\floyd\software\PrismSeq_Uploader\ /D /Y
+
+pause
